a79b390353732532260ec651e3486c033036a52b
Dave Taggart <taggart@applab.net>
2024-06-07 11:24:51 -0700

    Signed-off-by: Dave Taggart <taggart@applab.net>

---
+++ Scripts/npp_notepadplusplus/Executor.ps1
@@ -0,0 +1,9 @@
+﻿param ([string]$LocalLogsDir)
+$CommonPath = Join-Path -Path $PSScriptRoot -ChildPath "..\Common\Test.ps1"
+. $CommonPath
+
+$image = "npp/notepadplusplus"
+$using = "turbobuild/isolate-edge-wc"
+$isolate = "merge-user"
+
+StandardTest -LocalLogsDir $LocalLogsDir -image $image -using $using -isolate $isolate 
