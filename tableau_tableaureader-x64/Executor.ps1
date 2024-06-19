74f0b4371f6f88699b4fa3fc0bf836ff370a42c7
Huiyu He <huiyu@turbo.net>
2024-06-19 14:44:31 -0700

    Add test for Tableau Reader 64-bit

---
+++ tableau_tableaureader-x64/Executor.ps1
@@ -0,0 +1,11 @@
+﻿param (
+        [string]$localLogsDir        
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+$image = "tableau/tableaureader-x64"
+$using = "turbobuild/isolate-edge-wc"
+
+StandardTest -image $image -using $using -localLogsDir $localLogsDir
