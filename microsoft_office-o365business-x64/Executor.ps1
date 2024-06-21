06ec6517a57b699693414e1fb97af0cff5d80731
Huiyu He <huiyu@turbo.net>
2024-06-21 15:05:27 -0700

    Update tests for Office365 apps

---
+++ microsoft_office-o365business-x64/Executor.ps1
@@ -0,0 +1,12 @@
+﻿param (
+        [string]$localLogsDir        
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+$image = "microsoft/office-o365business-x64"
+$isolate = "merge-user"
+$extra = "--enable=disablefontpreload"
+
+StandardTest -image $image -isolate $isolate -localLogsDir $localLogsDir -extra $extra
