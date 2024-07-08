eede90e073c3ec59506727f318809dc982c36ade
Huiyu He <huiyu@turbo.net>
2024-07-08 13:45:21 -0700

    Add test for Ghidra

---
+++ nsa_ghidra/Executor.ps1
@@ -0,0 +1,15 @@
+﻿param (
+        [string]$localLogsDir        
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+$image = "nsa/ghidra"
+$using = "eclipse/temurin"
+$isolate = "merge-user"
+
+New-Item -Path "$env:USERPROFILE\Desktop\ghidra-test" -ItemType Directory
+New-Item -Path "$env:USERPROFILE\Desktop\ghidra-test-headless" -ItemType Directory
+
+StandardTest -image $image -using $using -isolate $isolate -localLogsDir $localLogsDir
