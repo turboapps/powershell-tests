53d5867c93e76706080298cc5ebdeb5b4659cfc8
Huiyu He <huiyu@turbo.net>
2024-07-24 11:58:39 -0700

    Optimize the script for putty/putty

--- putty_putty/Executor.ps1
+++ putty_putty/Executor.ps1
@@ -8,4 +8,4 @@ $IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
 
 $image = "putty/putty"
 
-StandardTest -image $image -isolate $isolate -extra $extra -localLogsDir $localLogsDir
+StandardTest -image $image -extra $extra -localLogsDir $localLogsDir
