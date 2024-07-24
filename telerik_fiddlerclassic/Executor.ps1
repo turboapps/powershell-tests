a9fdb158d8d3922e3c554ef9bd2fa888dc5f0822
Huiyu He <huiyu@turbo.net>
2024-07-24 13:28:54 -0700

    Add test for telerik/fiddlerclassic

---
+++ telerik_fiddlerclassic/Executor.ps1
@@ -0,0 +1,12 @@
+﻿param (
+        [string]$extra,
+        [string]$localLogsDir
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+$image = "telerik/fiddlerclassic"
+$using = "turbobuild/isolate-edge-wc"
+
+StandardTest -image $image -using $using -extra $extra -localLogsDir $localLogsDir
---
+++ telerik_fiddlerclassic/test.sikuli/test.py
@@ -0,0 +1,33 @@
+script_path = os.path.dirname(os.path.abspath(sys.argv[0])) 
+include_path = os.path.join(script_path, os.pardir, os.pardir, "!include", "util.sikuli")
+sys.path.append(include_path)
+import util
+reload(util)
+addImagePath(include_path)
+
+setAutoWaitTimeout(50)
+util.pre_test()
+
+# Test of `turbo run`.
+wait("fiddler_window.png")
+run("turbo stop test")
+
+# Launch the app.
+run("explorer " + os.path.join(util.start_menu, "Fiddler Classic.lnk"))
+wait("fiddler_window.png")
+
+# Basic operations.
+doubleClick(Pattern("captured.png").targetOffset(88,29))
+wait("inspection.png")
+
+# Check "help".
+type(Key.F1)
+util.close_firewall_alert()
+wait("help_url.png")
+closeApp("Edge")
+wait(5)
+type(Key.F4, Key.ALT)
+wait(10)
+
+# Check if the session terminates.
+util.check_running()
