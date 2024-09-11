f6a8a4b76900ce909c425398f6565ea4b97ada33
davetagg <124302945+davetagg@users.noreply.github.com>
2024-09-11 11:35:23 -0700

    Add test for Autodesk DWG TrueView (#35)
    * add-dwgtrueview-test
    
    * updated test scripts
    
    * Further update the test script
    
    ---------
    
    Co-authored-by: Huiyu He <huiyu@turbo.net>

---
+++ autodesk_dwgtrueview/Executor.ps1
@@ -0,0 +1,14 @@
+﻿param (
+        [string]$extra,
+        [string]$localLogsDir
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+$image = "autodesk/dwgtrueview"
+$using = "turbobuild/isolate-edge-wc"
+$isolate = "merge-user"
+$extra = "--enable=disablefontpreload " + $extra
+
+StandardTest -image $image -using $using -isolate $isolate -extra $extra -localLogsDir $localLogsDir
