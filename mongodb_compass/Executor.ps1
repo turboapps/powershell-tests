86e41bd2c11b363ecf6e19c6095a4fcb4bf649a9
davetagg <124302945+davetagg@users.noreply.github.com>
2024-09-20 16:46:07 -0700

    Add tests for mongodb compass and communityserver (#38)
    * add tests for mongodb compass and communityserver
    
    * updated test.py and executor.ps1 scripts
    
    * Edit some strings
    
    ---------
    
    Co-authored-by: Huiyu He <huiyu@turbo.net>

---
+++ mongodb_compass/Executor.ps1
@@ -0,0 +1,24 @@
+﻿param (
+        [string]$extra,
+        [string]$localLogsDir
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+$image = "mongodb/compass"
+$app = "mongodb/communityserver"
+
+PrepareTest -image $image -localLogsDir $localLogsDir
+
+# Run MongoDB to test MongoDB Compass.
+PullTurboImages -image $app
+RunTurboApp -image $app -extra "$extra -n=mongodbserver" -detached $True
+
+PullTurboImages -image $image
+InstallTurboApp -image $image -extra $extra
+TryTurboApp -image $image -extra $extra -detached $True
+HidePowerShellWindow
+$TestResult = StartTest -image $image -localLogsDir $localLogsDir
+
+return $TestResult
