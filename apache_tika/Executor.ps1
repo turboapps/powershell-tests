dd6fd79aaf38eaa4d024bbbd96fc3e1f9f04c0fa
Huiyu He <huiyu@turbo.net>
2024-09-23 13:36:53 -0700

    Fix java issue

--- apache_tika/Executor.ps1
+++ apache_tika/Executor.ps1
@@ -7,12 +7,12 @@ $IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
 . $IncludePath
 
 $image = "apache/tika"
-$using = "eclipse/temurin-lts"
+$using = "eclipse/temurinjre-lts"
 
 PrepareTest -image $image -localLogsDir $localLogsDir
 PullTurboImages -image $image -using $using
 InstallTurboApp -image $image -using $using
-TryTurboApp -image $image -extra "--startup-file=cmd $extra" -detached $True
+TryTurboApp -image $image -using $using -extra "--startup-file=cmd $extra" -detached $True
 HidePowerShellWindow
 $TestResult = StartTest -image $image -localLogsDir $localLogsDir
 
