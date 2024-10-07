716435da20ac54a361457bf2c93405fcca32715c
davetagg <124302945+davetagg@users.noreply.github.com>
2024-10-07 15:15:58 -0700

    add libreoffice test

---
+++ tdf_libreoffice/Executor.ps1
@@ -0,0 +1,14 @@
+﻿param (
+        [string]$extra,
+        [string]$localLogsDir
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+$image = "tdf/libreoffice"
+$using = "eclipse/temurinjre-lts,turbobuild/isolate-edge-wc"
+$extra = "--enable=disablefontpreload"
+$isolate = "merge-user"
+
+StandardTest -image $image -isolate $isolate -using $using -extra $extra -localLogsDir $localLogsDir
