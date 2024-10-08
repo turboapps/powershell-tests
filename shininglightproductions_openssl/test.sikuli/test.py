6abb5a2ccbebd4d8a3c21eb4114f33eb1e754eba
davetagg <124302945+davetagg@users.noreply.github.com>
2024-10-08 15:18:38 -0700

    added openssl test

---
+++ shininglightproductions_openssl/test.sikuli/test.py
@@ -0,0 +1,49 @@
+script_path = os.path.dirname(os.path.abspath(sys.argv[0])) 
+include_path = os.path.join(script_path, os.pardir, os.pardir, "!include", "util.sikuli")
+sys.path.append(include_path)
+import util
+reload(util)
+addImagePath(include_path)
+
+setAutoWaitTimeout(20)
+util.pre_test()
+
+# Test of `turbo run` and command line mode.
+wait("openssl-cmd-prompt.png")
+type("openssl help" + Key.ENTER)
+wait("openssl-help-cmd.png")
+wait(3)
+run("turbo stop test")
+
+# Launch the app.
+run("explorer " + os.path.join(util.start_menu, "OpenSSL", "Win64 OpenSSL Command Prompt.lnk"))
+wait("openssl-cmd-prompt.png")
+
+# Basic operations.
+certInFile = os.path.join(script_path, os.pardir, "resources", "certificate.pfx")
+certOutFile = os.path.join(util.desktop, "certificate.pem")
+openSSLcmd = "openssl pkcs12 -in " + certInFile + " -out " + certOutFile + " -nodes"
+type(openSSLcmd + Key.ENTER)
+wait("enter-password.png")
+type("thisismycertpassword" + Key.ENTER)
+wait(5)
+assert(util.file_exists(certOutFile, 10))
+with open(certOutFile, 'r') as file:
+    content = file.read()
+    assert("-----END PRIVATE KEY-----" in content)
+type("exit" + Key.ENTER)
+wait(5)
+
+# Test headless trigger
+certOutFile = os.path.join(util.desktop, "headless_cert.pem")
+headlessCmd = "turbo run openssl --isolate=merge-user --trigger headless -- pkcs12 -in " + certInFile + " -out " + certOutFile + " -nodes -password pass:thisismycertpassword"
+run(headlessCmd)
+wait(5)
+assert(util.file_exists(certOutFile, 10))
+with open(certOutFile, 'r') as file:
+    content = file.read()
+    assert("-----END PRIVATE KEY-----" in content)
+
+
+# Check if the session terminates.
+util.check_running()
