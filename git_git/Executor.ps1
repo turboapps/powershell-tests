b37f0c47e4996abeda4b275617154ea010b747fc
davetagg <124302945+davetagg@users.noreply.github.com>
2024-10-09 16:26:10 -0700

    added git test script

---
+++ git_git/Executor.ps1
@@ -0,0 +1,13 @@
+﻿param (
+        [string]$extra,
+        [string]$localLogsDir
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+$image = "git/git"
+$isolate = "merge+merge-user"
+$using = "turbobuild/isolate-edge-wc"
+
+StandardTest -image $image -using $using -isolate $isolate -extra $extra -localLogsDir $localLogsDir
