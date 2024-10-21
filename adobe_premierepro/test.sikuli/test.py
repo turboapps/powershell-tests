d59b4985529d5e36e1148de2321823733275b385
Huiyu He <huiyu@turbo.net>
2024-10-21 16:47:39 -0700

    Update the test for Adobe Premiere Pro

--- adobe_premierepro/test.sikuli/test.py
+++ adobe_premierepro/test.sikuli/test.py
@@ -15,21 +15,27 @@ username = credentials.get("username")
 password = credentials.get("password")
 
 # Test of `turbo run`.
+if exists("GPUSniffer_error.png"):
+    click(Pattern("GPUSniffer_error_close.png").targetOffset(-49,4))
 util.adobe_cc_login(username, password)
 wait("pr_window.png")
 run("turbo stop test")
 
 # Launch the app.
 run("explorer " + util.get_shortcut_path_by_prefix(util.start_menu, "Adobe Premiere Pro"))
+if exists("GPUSniffer_error.png"):
+    click(Pattern("GPUSniffer_error_close.png").targetOffset(-49,4))
 
 # Basic operations.
 wait("pr_window.png")
 type("o", Key.CTRL)
 wait("location.png")
 type(os.path.join(script_path, os.pardir, "resources", "create-project-import-media", "create-project-import-media-step1.prproj") + Key.ENTER)
-click(Pattern("convert_prompt.png").targetOffset(190,50))
+click(Pattern("convert_prompt.png").targetOffset(192,15))
 if exists("no_output_device.png"):
-    click(Pattern("no_output_device.png").targetOffset(126,40))
+    click(Pattern("no_output_device.png").targetOffset(135,55))
+if exists("renamed.png"):
+    click(Pattern("renamed.png").targetOffset(274,-25))
 wait("timeline.png")
 
 # Check "help".
@@ -39,6 +45,8 @@ App("Edge").focus() # Edge will lose focus after closing the firewall alert.
 wait("help_url.png")
 closeApp("Edge")
 wait(10)
+if exists("renamed.png"):
+    click(Pattern("renamed.png").targetOffset(274,-25))
 click("timeline.png") # Gain focus.
 type(Key.F4, Key.ALT)
 wait(10)
