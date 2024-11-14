98c531aac3474d5953d5c71ef8edcb9c524a4dea
davetagg <124302945+davetagg@users.noreply.github.com>
2024-11-13 17:14:34 -0800

    update llama test

--- ggerganov_llama.cpp/test.sikuli/test.py
+++ ggerganov_llama.cpp/test.sikuli/test.py
@@ -8,10 +8,18 @@ addImagePath(include_path)
 setAutoWaitTimeout(30)
 util.pre_test()
 
+# Test turbo run
+run("explorer " + os.path.join(util.start_menu,"System Tools","Command Prompt.lnk"))
+wait(5)
+type('turbo run ggerganov/llama.cpp --using=microsoft/vcredist --offline --name=test -- /C "C:\\llama-avx2\\llama-server.exe" -m C:\\llama-2-7b-chat.Q4_K_M.gguf -n 50 --port 8180 --chat-template llama2')
+type(Key.ENTER)
+
 # Test of the app.
 wait("ready.png", 120) # It takes time for the model to warm up.
 type(Key.DOWN, Key.WIN) # Hide the llama cli window.
 
+run("explorer " + os.path.join(util.start_menu,"System Tools","Command Prompt.lnk")) # launch another command prompt
+wait(5)
 click("cmd_window.png")
 curl_command = 'curl --request POST --url http://localhost:8180/v1/chat/completions --header "Content-Type: application/json" --data "{\\"messages\\": [{\\"role\\": \\"user\\", \\"content\\": \\"test\\"}]}"'
 type(curl_command + Key.ENTER)
@@ -21,8 +29,9 @@ type("exit" + Key.ENTER)
 App().focus("cmd.exe")
 App().focus("cmd.exe") # Make the llama cli window visable.
 wait("success.png")
+wait(5)
 run("turbo stop test")
 wait(10)
 
 # Check if the session terminates.
-assert("test" not in run("turbo sessions"))
+util.check_running()
