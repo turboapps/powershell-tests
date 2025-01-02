e38e522809814b141d5561ae82a7484065d12ea5
davetagg <124302945+davetagg@users.noreply.github.com>
2025-01-02 12:10:10 -0800

    added new test for Trillian

---
+++ cerulean_trillian/Executor.ps1
@@ -0,0 +1,11 @@
+﻿param (
+        [string]$extra,
+        [string]$localLogsDir
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+$image = "cerulean/trillian"
+
+StandardTest -image $image -using $using -localLogsDir $localLogsDir
