2e86726f2231ca578bd37177477d7209a326bd19
davetagg <124302945+davetagg@users.noreply.github.com>
2025-01-09 15:21:21 -0800

    added hec-ras test

---
+++ usace_hec-ras/Executor.ps1
@@ -0,0 +1,16 @@
+﻿param (
+        [string]$extra,
+        [string]$localLogsDir
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+$resources = Join-Path -Path $PSScriptRoot -ChildPath "resources"
+Copy-Item -Path (Join-Path -Path $resources -ChildPath "HEC Data") -Destination (Join-Path -Path ([Environment]::GetFolderPath("MyDocuments")) -ChildPath "HEC Data") -Recurse -Force
+
+
+$image = "usace/hec-ras"
+$isolate = "merge-user"
+
+StandardTest -image $image -isolate $isolate -extra $extra -localLogsDir $localLogsDir
