124ad4d8ab25f6a42889a3641450704daa138e04
davetagg <124302945+davetagg@users.noreply.github.com>
2025-01-10 15:26:23 -0800

    add test for treesize-free

---
+++ jamsoftware_treesize-free/Executor.ps1
@@ -0,0 +1,13 @@
+﻿param (
+        [string]$extra,
+        [string]$localLogsDir
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+$image = "jamsoftware/treesize-free"
+$isolate = "merge-user"
+$extra = "--mount=$env:LOCALAPPDATA\microsoft\windows\inetcache"
+
+StandardTest -image $image -isolate $isolate -extra $extra -localLogsDir $localLogsDir
