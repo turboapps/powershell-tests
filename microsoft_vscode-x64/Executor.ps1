b2047ff985aea95df93d1d8578d652f52d5e36a2
davetagg <124302945+davetagg@users.noreply.github.com>
2025-01-16 11:01:07 -0800

    Update Executor.ps1

--- microsoft_vscode-x64/Executor.ps1
+++ microsoft_vscode-x64/Executor.ps1
@@ -7,7 +7,7 @@ $IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
 . $IncludePath
 
 $image = "microsoft/vscode-x64"
-$using = "python/python-x64,eclipse/temurin-lts,microsoft/dotnet-sdk-x64,turbobuild/isolate-edge-wc"
+$using = "python/python-x64,eclipse/temurin-lts,microsoft/dotnet-sdk-x64:8,turbobuild/isolate-edge-wc"
 $isolate = "merge-user"
 
 StandardTest -image $image -using $using -isolate $isolate -extra $extra -localLogsDir $localLogsDir
