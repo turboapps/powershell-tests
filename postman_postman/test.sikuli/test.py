d75f2abb22615bd57507364e6f4fcca05401548a
davetagg <124302945+davetagg@users.noreply.github.com>
2025-01-17 09:47:31 -0800

    updated postman test

--- postman_postman/test.sikuli/test.py
+++ postman_postman/test.sikuli/test.py
@@ -9,16 +9,15 @@ setAutoWaitTimeout(30)
 util.pre_test()
 
 # Test of `turbo run`.
-wait("login-prompt.png")
+wait("light-client.png")
 run("turbo stop test")
 
 # Launch the app.
 run("explorer " + os.path.join(util.start_menu, "Postman", "Postman.lnk"))
 
 # Basic operations.
-wait("login-prompt.png")
-wait(3) # Wait for the animation to finish.
-click(Pattern("login-prompt.png").targetOffset(2,203))
+wait("light-client.png")
+click("light-client.png")
 wait("open-api-client.png")
 click("open-api-client.png")
 wait("get-url.png")
