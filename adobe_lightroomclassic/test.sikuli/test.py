d0900c7203d6cf8dfa144068deef19ac53cbd848
davetagg <124302945+davetagg@users.noreply.github.com>
2025-06-20 13:30:24 -0700

    updated lightroomclassic test

--- adobe_lightroomclassic/test.sikuli/test.py
+++ adobe_lightroomclassic/test.sikuli/test.py
@@ -22,62 +22,60 @@ run("explorer " + os.path.join(util.start_menu,"System Tools","Command Prompt.ln
 wait(5)
 type('turbo run lightroomclassic --using=creativeclouddesktop,isolate-edge-wc --offline --enable=disablefontpreload --name=test')
 type(Key.ENTER)
-if exists("share_data.png"):
-    click(Pattern("share_data.png").targetOffset(194,72))
-if exists("whats_new.png"):
+
+# Minimize the command prompt
+App().focus("Command Prompt")
+type(Key.DOWN, Key.WIN)
+
+if exists("whats_new_close.png",90):
     click("whats_new_close.png")
-if exists(Pattern("welcome.png").targetOffset(71,3)):
-    click(Pattern("welcome.png").targetOffset(71,3))
-if exists("getting_started.png"):
+if exists("getting_started.png",15):
     click(Pattern("getting_started.png").targetOffset(186,-18))
 wait(15)
 run("turbo stop test")
+wait(10)
 
 # Launch the app.
 run("explorer " + os.path.join(util.start_menu, "Adobe Lightroom Classic.lnk"))
-if exists("share_data.png"):
-    click(Pattern("share_data.png").targetOffset(194,72))
-if exists("whats_new.png"):
+if exists("whats_new_close.png",90):
     click("whats_new_close.png")
-if exists(Pattern("welcome.png").targetOffset(71,3)):
-    click(Pattern("welcome.png").targetOffset(71,3))
-if exists("getting_started.png"):
+if exists("getting_started.png",15):
     click(Pattern("getting_started.png").targetOffset(186,-18))
 
 # Basic operations.
-wait("lightroomcc_window.png")
+wait("lightroomcc_window.png",30)
 type("i", Key.CTRL + Key.SHIFT)
 click("import_select_source.png")
-click(Pattern("import_select_source_menu.png").targetOffset(-8,7))
-wait("import_location.png")
+click("import_select_source_menu.png")
+wait("import_location.png",20)
 type(os.path.join(script_path, os.pardir, "resources") + Key.ENTER)
 wait(2)
 type(Key.ENTER)
-wait("import_thumbnail.png")
+wait("import_thumbnail.png",10)
 click("import_import.png")
-wait("import_done.png")
+wait("import_done.png",20)
 type("d")
 wait(2) # Wait for Edit panel to load.
-if exists("develop_mode.png"):
+if exists("develop_mode.png",30):
     click(Pattern("develop_mode.png").targetOffset(181,-96))
-if exists("point_color.png"):
+if exists("point_color.png",15):
     click(Pattern("point_color.png").targetOffset(219,-54))
 type("r")
 wait(2)
 type("x")
 wait(2)
 type(Key.ENTER)
-wait("adjusted.png")
+wait("adjusted.png",15)
 type("e", Key.CTRL + Key.SHIFT)
-wait("export.png")
+wait("export.png",15)
 type(Key.ENTER)
 
 # Check "help".
 type(Key.F1)
-wait("help_url.png")
+wait("help_url.png",30)
 closeApp("Edge")
 wait(10)
-if exists("import_select_source.png"):
+if exists("import_select_source.png",10):
     wait(2)
     click("import_cancel.png")
 type("q", Key.CTRL)
