00b46dd5f27a1f27de76892262bce5f04d3f4503
davetagg <124302945+davetagg@users.noreply.github.com>
2025-09-19 12:32:54 -0700

    Update Executor.ps1

--- apple_itunes-x64/Executor.ps1
+++ apple_itunes-x64/Executor.ps1
@@ -9,4 +9,7 @@ $IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
 $image = "apple/itunes-x64"
 $isolate = "merge-user"
 
+# Disable the firewall to prevent the prompt
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
 StandardTest -image $image -isolate $isolate -extra $extra -localLogsDir $localLogsDir
