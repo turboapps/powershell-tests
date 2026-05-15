04db8d19ff49583a4a23ba5c247d4e9b334d8222
davetagg <124302945+davetagg@users.noreply.github.com>
2026-05-15 13:11:38 -0700

    update github and r tests

--- github_githubdesktop/Executor.ps1
+++ github_githubdesktop/Executor.ps1
@@ -6,6 +6,10 @@
 $IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
 . $IncludePath
 
+if (Test-Path "$env:USERPROFILE\.gitconfig") {
+    Remove-Item "$env:USERPROFILE\.gitconfig" -Force -ErrorAction SilentlyContinue
+}
+
 $image = "github/githubdesktop"
 $using = "turbobuild/isolate-edge-wc"
 $isolate = "merge+merge-user"
