894ce1621b389328befb7b2df02e9c498a954ce6
davetagg <124302945+davetagg@users.noreply.github.com>
2026-09-04 10:50:04 -0700

    added vsbuildtools test and updated nodejs tests

---
+++ microsoft_vsbuildtools/Executor.ps1
@@ -0,0 +1,18 @@
+﻿param (
+        [string]$extra,
+        [string]$localLogsDir
+    )
+
+$IncludePath = Join-Path -Path $PSScriptRoot -ChildPath "..\!include\Test.ps1"
+. $IncludePath
+
+# Create firewall rules to prvent prompt
+New-NetFirewallRule -DisplayName "Allow node" -Direction Inbound -Protocol TCP -LocalPort 3000 -Action Allow -Profile Any
+New-NetFirewallRule -DisplayName "Allow node" -Direction Inbound -Protocol UDP -LocalPort 3000 -Action Allow -Profile Any
+
+$image = "microsoft/vsbuildtools"
+$app = "nodejs/nodejs"
+$using = "python/python-x64,microsoft/vsbuildtools"
+$extra = $extra + " --enable=usedllinjection --working-dir=" + $PSScriptRoot + "\resources "
+
+StandardTest -image $app -using $using -isolate $isolate -extra $extra -shouldInstall $False -localLogsDir $localLogsDir
---
+++ microsoft_vsbuildtools/test.sikuli/test.py
@@ -0,0 +1,39 @@
+# The tests for nodejs/nodejs and nodejs/nodejs-lts are the same.
+
+script_path = os.path.dirname(os.path.abspath(sys.argv[0])) 
+include_path = os.path.join(script_path, os.pardir, os.pardir, "!include", "util.sikuli")
+sys.path.append(include_path)
+import util
+reload(util)
+addImagePath(include_path)
+
+setAutoWaitTimeout(20)
+util.pre_test(no_min=True)
+
+# Test cpp nodejs project.
+type('cd my-node-cpp-app' + Key.ENTER)
+wait(3)
+type('npm install node-addon-api@8' + Key.ENTER)
+wait(30)
+type('npx --yes node-gyp configure' + Key.ENTER)
+wait(40)
+type('npx --yes node-gyp build' + Key.ENTER)
+wait(40)
+type('node index.js' + Key.ENTER)
+wait(20)
+wait("indexjs-result.png",60)
+
+# Test standard nodejs project.
+type('node ..\my-node-app\\app.js' + Key.ENTER)
+wait("server-running.png")
+run('explorer "http://localhost:3000"')
+wait("localhost.png")
+wait(5)
+closeApp("Edge")
+wait(5)
+os.system('cmd /c taskkill /f /im "vctip.exe" /t')
+run("turbo stop test")
+wait(10)
+
+# Check if the session terminates.
+assert("test" not in run("turbo sessions"))
